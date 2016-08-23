22b61edf7a92f83da4ba01819f5d519d394183a7
abhishekgupta19 <Abhishek.Gupta@crest.in>
2016-08-23 13:40:30 +0530

    Add files via upload

---
+++ EulerSolution1.scala
@@ -0,0 +1,23 @@
+/*
+  Sum of Natural Number below 10 which are
+  multiple by 3 or 5
+
+ */
+
+object EulerSolution1 extends App{
+
+  euler_solution1()
+
+  def euler_solution1(): Unit = {
+
+    var s : Int = 0
+    val list:Range= 1 until 1000
+    for(i <- list
+        if i % 3 == 0 || i % 5 == 0 )
+    {
+      s = s + i
+    }
+
+    println("sum of natural number below 10 which are multiple by 3 or 5\n" + s)
+  }
+}
